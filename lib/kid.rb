ab980133be89d8a7e90a8a1d911342ede984f67b
Vianka Rivera <viankalrivera@gmail.com>
2020-05-20 20:29:42 +0000

    Done.

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,2 +1,9 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
+require_relative './fancy_dance.rb'
+
+class Kid
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
+
+end
